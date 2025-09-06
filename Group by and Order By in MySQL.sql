95d381083b11dc41de6b41f707a9c5d6642efc79
Joy Olusanya <91166707+joynaomi81@users.noreply.github.com>
2025-09-06 23:56:48 +0100

    Add files via upload

---
+++ Group by and Order By in MySQL.sql
@@ -0,0 +1,30 @@
+-- Group By
+SELECT *
+FROM employee_demographics;
+
+SELECT gender
+FROM employee_demographics
+GROUP BY gender
+;
+
+SELECT gender, AVG (age), MAX(age), MIN(age), COUNT(age)
+FROM employee_demographics
+GROUP BY gender
+;
+
+SELECT occupation, salary
+FROM employee_salary
+GROUP BY occupation, salary
+;
+
+
+-- ORDER BY
+SELECT *
+FROM employee_demographics
+ORDER BY first_name
+;
+
+SELECT *
+FROM employee_demographics
+ORDER BY age, gender
+;
